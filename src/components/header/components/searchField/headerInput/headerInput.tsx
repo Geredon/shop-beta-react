4f5eed58d4da7161781bdf31af4b5488ffa8f5e4
Geredon <walttt@yandex.ru>
2023-02-20 10:20:14 +0300

    hw7.Реализовал material ui

--- src/components/header/components/searchField/headerInput/headerInput.tsx
+++ src/components/header/components/searchField/headerInput/headerInput.tsx
@@ -1,4 +1,4 @@
-import style from "./headerInput.module.css"
+
 import {setSearchValue} from "../../../../../store/common/actionCreators";
 import {debounce} from "../../../../../lib/debounce";
 import {useDispatch} from "react-redux";
@@ -9,10 +9,10 @@ const HeaderInput = () => {
     const dispatch = useDispatch()
 
     return (
-        <div className={style.headerInput}>
-            <TextField variant="standard"  InputProps={{ disableUnderline: true }}
-                     onChange={debounce((event: any) => dispatch(setSearchValue(event.target.value)), 300)}
-                     placeholder="Search products"/>
+        <div>
+             <TextField variant="standard"  InputProps={{ disableUnderline: true }} fullWidth sx={{width:"156px"}}
+                        onChange={debounce((event: any) => dispatch(setSearchValue(event.target.value)), 300)}
+                        placeholder="Search products"/>
         </div>
     )
 }
--- src/pages/detailed/components/detailedInfo/cartAdd/cartBtnAdd/CartBtnAdd.tsx
+++ src/pages/detailed/components/detailedInfo/cartAdd/cartBtnAdd/CartBtnAdd.tsx
@@ -1,12 +1,21 @@
 import style from "./CartBtnAdd.module.css"
-import {Button} from "@mui/material";
+import {Box, Button} from "@mui/material";
 
 const CartBtnAdd = () => {
     return (
         <div className={style.cartBtnAdd}>
-            <Button>
-                Add to cart
-            </Button>
+              <Button sx={{
+                  width: "160px",
+                  height: "40px",
+                  fontSize: "24px",
+                  color:" #ffffff",
+                  background: "var(--orange)",
+                  border: "none",
+                  borderRadius: "10px",
+                  textTransform: "none",
+              }}>
+                  Add to cart
+              </Button>
         </div>
     )
 };
