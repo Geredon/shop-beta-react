fd97750711cdf5e0f63afe08f23e0a8e235ae1af
Geredon <walttt@yandex.ru>
2023-02-26 08:23:59 +0300

    hw7.Добавил Grid и тему из material

--- src/pages/catalog/components/product/Product.tsx
+++ src/pages/catalog/components/product/Product.tsx
@@ -5,26 +5,29 @@ import ProductTitle from "./productTitle/ProductTitle";
 import ProductPrice from "./productPrice/ProductPrice";
 import {NavLink} from "react-router-dom";
 import {PATH_DETAILED} from "../../../../constants/constants";
-import {FC} from "react";
+import React, {FC} from "react";
 import {TCatalogData} from "../../../../api/api";
+import {Grid} from "@mui/material";
 
 
 type PropsProductType = Omit<TCatalogData, "description">
 
 
-const Product:FC<PropsProductType> = ({id, like,picture,name,price}) => {
+const Product: FC<PropsProductType> = ({id, like, picture, name, price}) => {
 
     const productPath = PATH_DETAILED.replace(':id', id!)
 
     return (
-        <li className={style.product}>
-            <NavLink to={productPath}>
-                <Favorite like={like}/>
-                <ProductImg path={picture.path} alt={picture.alt}/>
-                <ProductTitle name={name}/>
-                <ProductPrice value={price.value} currency={price.currency} />
-            </NavLink>
-        </li>
+        <Grid item lg={2} md={3} sm={4} xs={6}>
+            <li className={style.product}>
+                <NavLink to={productPath}>
+                    <Favorite like={like}/>
+                    <ProductImg path={picture.path} alt={picture.alt}/>
+                    <ProductTitle name={name}/>
+                    <ProductPrice value={price.value} currency={price.currency}/>
+                </NavLink>
+            </li>
+        </Grid>
     )
 }
 export default Product;
